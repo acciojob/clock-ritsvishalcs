6dca631f21967fff07b42b964cfbb675b4698920
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-23 17:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const displayTimer = document.getElementById('timer');
+
+setInterval(function () {
+    const updateTimer = new Date();
+    displayTimer.innerHTML = updateTimer.toLocaleTimeString();
+}, 1000);
